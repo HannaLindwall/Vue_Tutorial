7eab6e1313cf7ebbf23e6ea485568b398ee11393
Hanna Lindwall <hanna.lindwall93@gmail.com>
2020-04-23 15:56:47 +0200

    fix styling for nav when router-lik is active

--- collective-helper/src/App.vue
+++ collective-helper/src/App.vue
@@ -4,9 +4,9 @@
       <img src="./assets/kollektivlogocropped.png" width="50%" class="center">
     </a>
     <nav>
-      <router-link to="/home">Home</router-link>
-      <router-link to="/laundry">Laundry</router-link>
-      <router-link to="/cleaning">Cleaning</router-link>
+      <router-link :style="homeIsActive" to="/home" v-on:click.native="isActive()">Home</router-link>
+      <router-link :style="laundryIsActive" to="/laundry" v-on:click.native="isActive()">Laundry</router-link>
+      <router-link :style="cleaningIsActive" to="/cleaning" v-on:click.native="isActive()">Cleaning</router-link>
     </nav>
     <router-view />
   </div>
@@ -16,6 +16,44 @@
 
 export default {
   name: 'App',
+  data() {
+    return {
+      homeIsActive: {
+      },
+      laundryIsActive: {
+      },
+      cleaningIsActive: {
+      }
+    }
+  },
+  created() {
+    var current = this.$router.currentRoute.name
+    if (current === "home" || current === "default") {
+        this.homeIsActive = {fontWeight: "bold"}
+      } else if (current === "laundry") {
+        this.laundryIsActive = {fontWeight: "bold"}
+      } else if (current === "cleaning") {
+        this.cleaningIsActive = {fontWeight: "bold"}
+      }
+  },
+  methods: {
+    isActive() {
+      var current = this.$router.currentRoute.name
+      if (current === "home" || current === "default") {
+        this.homeIsActive = {fontWeight: "bold"}
+        this.laundryIsActive = {fontWeight: "normal"}
+        this.cleaningIsActive = {fontWeight: "normal"}
+      } else if (current === "laundry") {
+        this.homeIsActive = {fontWeight: "normal"}
+        this.laundryIsActive = {fontWeight: "bold"}
+        this.cleaningIsActive = {fontWeight: "normal"}
+      } else if (current === "cleaning") {
+        this.homeIsActive = {fontWeight: "normal"}
+        this.laundryIsActive = {fontWeight: "normal"}
+        this.cleaningIsActive = {fontWeight: "bold"}
+      }
+    }
+  }
 }
 </script>
 
